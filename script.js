30db91ead19419cce589f707104363cbd886fdde
lakhman108 <119241790+lakhman108@users.noreply.github.com>
2023-09-10 16:36:45 +0530

    some minor changes

--- script.js
+++ script.js
@@ -1,20 +1,26 @@
 let changeborder = document.getElementById("card");
 console.log(changeborder);
-
-changeborder.addEventListener("mouseover", function () {
-  function getRandomColor() {
-    var letters = "0123456789ABCDEF";
-    var color = "#";
-    for (let i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
+let id;
+function getRandomColor() {
+  var letters = "0123456789ABCDEF";
+  var color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
   }
+  return color;
+}
+changeborder.addEventListener("mouseover", function () {
+  
   function changecolor() {
     const color = getRandomColor();
     changeborder.style.border = `5px solid ${color}`;
 
     changeborder.style.boxShadow = `0px 10px 15px 0 ${color}`;
   }
-  setInterval(changecolor, 1000);
+  id=setInterval(changecolor, 1000);
+});
+changeborder.addEventListener("mouseout", function () {
+  clearInterval(id);
+  changeborder.style.border = "none";
+  changeborder.style.boxShadow = "none";
 });
